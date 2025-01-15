b7199b98eb2704ad8f2dbe735b52371e67703222
BugHunter-X <askzenai@gmail.com>
2025-01-15 19:02:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,18 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect: missing return statement
+    fetch('https://api.example.com/data')
+      .then(response => response.json())
+      .then(data => {
+        setCount(data.count);
+      });
+  }, []);
+
+  return (
+    <div>Count: {count}</div>
+  );
+}
+```
